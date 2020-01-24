f12c1c4857fe4b62805d9734281349d761c87573
Manikandan <manikandan.m@10decoders.in>
2020-01-24 20:34:49 +0530

    commit

--- SpringOne/src/main/java/com/example/SpringOneApplication.java
+++ SpringOne/src/main/java/com/example/SpringOneApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class SpringOneApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringOneApplication.class, args);
 	}
